ec9b78eea10209bc68f4965a8798bc980763cde0
Saiket Das <saiketdas02@gmail.com>
2022-12-08 12:35:35 +0600

    Frontend - Redux actionTypes created

---
+++ frontend/src/redux/actionTypes/actionTypes.js
@@ -0,0 +1,8 @@
+export const ADD_CONTENT = "ADD_CONTENT";
+export const GET_CONTENT = "GET_CONTENT";
+export const UPDATE_CONTENT = "UPDATE_CONTENT";
+export const DELETE_CONTENT = "DELETE_CONTENT";
+
+// For Filtering
+export const TOGGLE_DATE = "TOGGLE_DATE";
+export const TOGGLE_CATEGORY = "TOGGLE_CATEGORY";
