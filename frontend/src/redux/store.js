3749e35fba9a3d97daa006a4a114205f34b8a78f
Saiket Das <saiketdas02@gmail.com>
2022-12-14 17:03:36 +0600

    Frontend - Middlewares deleted

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,10 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import thunk from "redux-thunk";
 import rootReducers from "./reducers/rootReducers";
-import contentCounter from "./middlewares/contentCounter";
 
 const store = createStore(
   rootReducers,
-  composeWithDevTools(applyMiddleware(contentCounter, thunk))
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 export default store;
